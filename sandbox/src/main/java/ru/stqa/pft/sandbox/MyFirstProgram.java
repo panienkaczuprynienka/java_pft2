fce66469e88283608faecb396ceb2f5f617d1ffa
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2017-10-05 17:51:07 +0200

    uporzadkowanie klas

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  /*public static void main(String[] args) {
+  public static void main(String[] args) {
     hello("jozek");
     hello("wiesiek");
     hello("marek");
@@ -19,26 +19,7 @@ public class MyFirstProgram {
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-}*/
-
-
-  public static double distance(Point p1, Point p2) {
-
-    double dx;
-    double dy;
-    dx = p2.x - p1.x;
-    dy = p2.y - p1.y;
-
-    return Math.sqrt((dx * dx) + (dy * dy));
-  }
-
-  public static void main(String[] args) {
-    Point p1 = new Point(4.0,2.0);
-    Point p2 = new Point(8.0,4.0);
-
-
-    System.out.println("Dlugość odcinka o współrzędnych " + p1.x +" "+ p1.y + " oraz " + p2.x +" "+ p2.y + " wynosi " + distance(p1, p2));
-
-  }
 }
+
+
 
