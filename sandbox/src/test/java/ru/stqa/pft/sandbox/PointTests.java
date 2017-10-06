9c1494640c6669e3df1235ce011b8d845d4e2bf0
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2017-10-06 13:41:22 +0200

    Testy dla klasy Point (zajęcia 2. zadanie1)

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance(){
+    Point p1 = new Point (2.0,10.0);
+    Point p2 = new Point (2.0,1.0);
+    Point p3 = new Point (20.0,20.0);
+
+    Assert.assertEquals(9.0, p1.distance(p2));
+
+    Assert.assertNotEquals(0.0,p1.distance(p2),"Test sprawdził,że długość istnieje (nie jest zerowa)");
+
+    Assert.assertNotNull(p1.distance(p2),"Wartość długości nie jest zerowa");
+
+    Assert.assertNotSame(p1.distance(p2),p1.distance(p3));
+
+    Assert.assertTrue(p1.distance(p3)>10.0);
+
+
+  }
+}
+
+
