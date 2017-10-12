a58f61a9b5683385c6c4e056d27392616553d272
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2017-10-12 17:53:45 +0200

    testy rozszerzone klasy Equation

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -16,10 +16,21 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (d > 0){
-      n = 2;
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
+
     } else {
-      if (d == 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
@@ -27,7 +38,7 @@ public class Equation {
     }
   }
 
-  public int rootNumber(){
+  public int rootNumber() {
     return n;
   }
 }
