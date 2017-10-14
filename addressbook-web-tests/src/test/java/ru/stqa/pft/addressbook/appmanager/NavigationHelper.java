a81d226f38f8a2598325f0973de339a1d953faf5
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2017-10-14 12:14:27 +0200

    Optymalizacja przejsc - NavigationHelper - leniwe działania

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -12,12 +12,17 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupPage() {
-
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    } else click(By.linkText("groups"));
   }
 
-  public void goToHomePage(){
-    click(By.linkText("home"));
+  public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    } else click(By.linkText("home"));
   }
 
 }
