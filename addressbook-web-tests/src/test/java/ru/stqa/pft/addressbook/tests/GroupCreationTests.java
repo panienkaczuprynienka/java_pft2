e3686ae2a7eb446583e0497add970d98a0478762
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2017-10-21 16:59:48 +0200

    GroupCreationTest z wyrazeniem lambda

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", "jop", "hop");
+    GroupData group = new GroupData("niewierzezetodziala", "jop", "hop");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size()+1);
@@ -25,7 +26,8 @@ public class GroupCreationTests extends TestBase{
         max=g.getId();
       }
     }
-    group.setId(max);
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object> (after));
   }
