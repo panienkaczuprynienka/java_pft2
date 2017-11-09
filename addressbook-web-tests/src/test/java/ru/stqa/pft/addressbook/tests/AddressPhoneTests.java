044f6ab18d590f2d78fbadda18d97d5fe68f5f9b
panienkaczuprynienka <panienkaczuprynienka@gmail.com>
2017-11-09 21:04:03 +0100

    assercja do zadania 13 ale wymagajace jeszcze poprawy

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressPhoneTests.java
@@ -26,19 +26,33 @@ public class AddressPhoneTests extends TestBase {
     AddressData address = app.address().all().iterator().next();
 
     AddressData addressInfoFromEditForm = app.address().infoFromEditForm(address);
-
     String addressInfoFromDetailForm = app.address().infoFromDetailForm(address);
+
+
     System.out.println(addressInfoFromDetailForm);
 
     assertThat(address.getAllPhones(), equalTo(mergePhones(addressInfoFromEditForm)));
     assertThat(address.getAllEmails(), equalTo(mergeEmails(addressInfoFromEditForm)));
     assertThat(cleanedPersonalAddress(address.getPersonalAddress()), equalTo(cleanedPersonalAddress((addressInfoFromEditForm.getPersonalAddress()))));
+// PONIŻEJ ZNAJDUJE SIĘ KLUCZOWA ASERCJA 
+    // assertThat(cleanedAllDetail(addressInfoFromDetailForm), equalTo(mergeAll(addressInfoFromEditForm)));
+  }
+
+  private String mergeAll(AddressData address) {
+    return Arrays.asList(address.getFirstname(), address.getMiddlename(), address.getLastname(), address.getNickname(),
+            address.getTitle(), address.getCompany(),
+            address.getPersonalAddress(),
+            address.getHomePhone(), address.getMobilePhone(), address.getWorkPhone(), address.getFaxPhone(),
+            address.getEmail(), address.getEmail2(), address.getEmail3(),
+            address.getHomepage())
+            .stream().filter((s) -> s != null && !s.equals(""))
+            .map(AddressPhoneTests::cleanedAllEdit)
+            .collect(Collectors.joining("\n"));
 
   }
 
-
   private String mergePhones(AddressData address) {
-    return Arrays.asList(address.getHomePhone(), address.getMobilePhone(), address.getWorkPhone(), address.getFaxPhone())
+    return Arrays.asList(address.getHomePhone(), address.getMobilePhone(), address.getWorkPhone())
             .stream().filter((s) -> !s.equals(""))
             .map(AddressPhoneTests::cleanedPhones)
             .collect(Collectors.joining("\n"));
@@ -56,6 +70,15 @@ public class AddressPhoneTests extends TestBase {
     return personalAddress.replaceAll("\\s", "").replaceAll("\\n", "");
   }
 
+  // OCZYSZCZANIE DANYCH Z EDIT FORM
+  public static String cleanedAllEdit(String addressInfoFromEditForm) {
+    return addressInfoFromEditForm.replaceAll("\\s", " ").replaceAll("\\n", " ");
+  }
+
+  // OCZYSZCZANIE DANYCH Z DETAIL FORM
+  public static String cleanedAllDetail(String addressInfoFromDetailForm) {
+    return addressInfoFromDetailForm.replaceAll("\\s", " ").replaceAll("\\n", " ");
+  }
   public static String cleanedPhones(String phone) {
 
     return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
